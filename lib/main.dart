da6b1dee644698fe7784a7070ad2d9e24178b505
Niku paswan <nikupaswan200@gmail.com>
2021-06-12 14:05:22 +0530

    buildingWidgetTree

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,37 @@
 import 'package:flutter/material.dart';
-void main() {
-  
-}
+
+void main() {}
+
 // ignore: use_key_in_widget_constructors
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    
     return const MaterialApp(
       title: 'The Quiz',
-      home:  Text('Home'),
+      home: Text('Home'),
+      /**
+       * 
+       * 
+       * Like we have a class  as person 
+       * 
+       * class person {
+       * // person class have some varable 
+       * //like this
+       * int age;
+       * String name;
+       * 
+       * /**
+       * remember class name is known as constractor
+       *  */
+
+       * //unName constractor
+       * person(this.age,this.name);
+       * name constractor // always add required inside the constractor
+       * for example
+       * Person({required this.age  require this.name});
+       * 
+       * }
+       */
     );
   }
 }
