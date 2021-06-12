654340fd745b6cda19214562c9a8d7f8812abe12
Niku paswan <nikupaswan200@gmail.com>
2021-06-12 15:40:09 +0530

    addingLayout

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
-void main() {}
+void main() {
+  runApp(MyApp());
+}
 
 // ignore: use_key_in_widget_constructors
 class MyApp extends StatelessWidget {
@@ -15,41 +17,14 @@ class MyApp extends StatelessWidget {
           ),
         ),
         body: Column(
-  children: const [
-    Text('Deliver features faster'),
-    Text('Craft beautiful UIs'),
-    Expanded(
-      child: FittedBox(
-        fit: BoxFit.contain, // otherwise the logo will be tiny
-        child: FlutterLogo(),
-      ),
-    ),
+  children:  [
+    
+    
+   
   ],
 )
       ),
-      /**
-       * 
-       * 
-       * Like we have a class  as person 
-       * 
-       * class person {
-       * // person class have some varable 
-       * //like this
-       * int age;
-       * String name;
-       * 
-       * /**
-       * remember class name is known as constractor
-       *  */
-
-       * //unName constractor
-       * person(this.age,this.name);
-       * name constractor // always add required inside the constractor
-       * for example
-       * Person({required this.age  require this.name});
-       * 
-       * }
-       */
+   
     );
   }
 }
